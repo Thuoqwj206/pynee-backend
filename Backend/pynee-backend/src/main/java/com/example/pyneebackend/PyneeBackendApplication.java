85271de31e403d4ac91aea62de2b080334b7dda5
thuongpv <88428890+Thuoqwj206@users.noreply.github.com>
2023-09-12 22:42:58 +0700

    initial commit

---
+++ Backend/pynee-backend/src/main/java/com/example/pyneebackend/PyneeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pyneebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyneeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyneeBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/pynee-backend/src/test/java/com/example/pyneebackend/PyneeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pyneebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyneeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
