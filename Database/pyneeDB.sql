c74b0128b0e1c87d5814d4a8b7177099542211f0
thuongpv <88428890+Thuoqwj206@users.noreply.github.com>
2023-09-13 17:27:41 +0700

    update database

--- Database/pyneeDB.sql
+++ Database/pyneeDB.sql
@@ -7,17 +7,18 @@ USE `PyneeDB`;
 
 -- Create the Account table
 CREATE TABLE `account` (
-    `account_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `account_id` INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
     `role` ENUM('customer', 'admin', 'touroperator', 'employee') NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL
 );
 
 -- Create the Customer Profile table
 CREATE TABLE `customer` (
     `customer_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `account_id` INT NOT NULL,
+    `account_id` INT NOT NULL UNIQUE,
     `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
     `address` VARCHAR(255),
     `phone` VARCHAR(20),
     FOREIGN KEY (`account_id`) REFERENCES `account`(`account_id`)
@@ -27,7 +28,9 @@ CREATE TABLE `touroperator` (
     `touroperator_id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `description` TEXT,
-    `status` ENUM('active', 'closed') NOT NULL
+     `account_id` INT NOT NULL UNIQUE,
+    `status` ENUM('active', 'closed') NOT NULL,
+    FOREIGN KEY (`account_id`) references `account`(`account_id`)
 );
 -- Create the Tour and Ticket table
 CREATE TABLE `tour` (
@@ -76,7 +79,7 @@ CREATE TABLE `feedback` (
 -- Create the Employee table
 CREATE TABLE `employee` (
     `employee_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `account_id` INT NOT NULL,
+    `account_id` INT NOT NULL UNIQUE,
     `employee_name` VARCHAR(255) NOT NULL,
     `status` ENUM('active', 'suspended') NOT NULL,
     
@@ -94,63 +97,72 @@ CREATE TABLE `booking` (
     FOREIGN KEY (`tour_id`) REFERENCES `tour`(`tour_id`),
     FOREIGN KEY (`customer_id`) REFERENCES `customer`(`customer_id`)
 );
--- Sample data for the 'account' table
-INSERT INTO `account` (`role`, `email`, `password`) VALUES
-    ('customer', 'customer1@example.com', 'password1'),
-    ('customer', 'customer2@example.com', 'password2'),
-    ('admin', 'admin@example.com', 'adminpassword'),
-    ('touroperator', 'operator1@example.com', 'operatorpassword1'),
-    ('touroperator', 'operator2@example.com', 'operatorpassword2'),
-    ('employee', 'employee1@example.com', 'employeepassword1'),
-    ('employee', 'employee2@example.com', 'employeepassword2'),
-    ('customer', 'customer3@example.com', 'password3'),
-    ('admin', 'admin2@example.com', 'adminpassword2'),
-    ('touroperator', 'operator3@example.com', 'operatorpassword3');
 
--- Sample data for the 'customer' table
-INSERT INTO `customer` (`account_id`, `name`, `address`, `phone`) VALUES
-    (1, 'Customer 1', '123 Main St', '555-1234'),
-    (2, 'Customer 2', '456 Oak Ave', '555-5678'),
-    (8, 'Customer 3', '789 Elm Rd', '555-9876'),
-    (9, 'Admin Customer', '101 Admin Ln', '555-1010');
+-- Insert values into the account table
+INSERT INTO `account` (`role`, `username`, `password`)
+VALUES
+    ('customer', 'user1', 'password1'),
+    ('admin', 'admin1', 'adminpass'),
+    ('touroperator', 'operator1', 'operatorpass'),
+    ('employee', 'employee1', 'employeepass'),
+    ('customer', 'user2', 'password2'),
+    ('customer', 'user3', 'password3'),
+    ('touroperator', 'operator2', 'operatorpass2'),
+    ('employee', 'employee2', 'employeepass2'),
+    ('customer', 'user4', 'password4'),
+    ('admin', 'admin2', 'adminpass2');
 
--- Sample data for the 'touroperator' table
-INSERT INTO `touroperator` (`name`, `description`, `status`) VALUES
-    ('Operator 1', 'Description for Operator 1', 'active'),
-    ('Operator 2', 'Description for Operator 2', 'closed'),
-    ('Operator 3', 'Description for Operator 3', 'active');
+-- Insert values into the customer table
+INSERT INTO `customer` (`account_id`, `name`, `email`, `address`, `phone`)
+VALUES
+    (1, 'John Doe', 'john@example.com', '123 Main St', '123-456-7890'),
+    (5, 'Jane Smith', 'jane@example.com', '456 Elm St', '987-654-3210'),
+    (6, 'Bob Johnson', 'bob@example.com', '789 Oak St', '555-555-5555'),
+    (9, 'Alice Brown', 'alice@example.com', '101 Pine St', '111-222-3333'),
+    (10, 'Eve Wilson', 'eve@example.com', '222 Cedar St', '444-444-4444');
 
--- Sample data for the 'tour' table
-INSERT INTO `tour` (`name`, `description`, `touroperator_id`, `status`) VALUES
+-- Insert values into the touroperator table
+INSERT INTO `touroperator` (`name`, `description`, `account_id`, `status`)
+VALUES
+    ('TourCo1', 'A tour operator description', 3, 'active'),
+    ('TourCo2', 'Another tour operator', 7, 'active');
+
+-- Insert values into the tour table
+INSERT INTO `tour` (`name`, `description`, `touroperator_id`, `status`)
+VALUES
     ('Tour 1', 'Description for Tour 1', 1, 'available'),
     ('Tour 2', 'Description for Tour 2', 1, 'available'),
-    ('Tour 3', 'Description for Tour 3', 2, 'unavailable');
+    ('Tour 3', 'Description for Tour 3', 2, 'available');
 
--- Sample data for the 'tour_for_sale' table
-INSERT INTO `tour_for_sale` (`tour_id`, `price`, `departure_date`, `quantity`, `status`) VALUES
-    (1, 100.00, '2023-09-15', 10, 'active'),
-    (2, 150.00, '2023-09-20', 5, 'active'),
-    (3, 200.00, '2023-09-25', 8, 'cancelled');
+-- Insert values into the tour_for_sale table
+INSERT INTO `tour_for_sale` (`tour_id`, `price`, `departure_date`, `quantity`, `status`)
+VALUES
+    (1, 100.00, '2023-10-15', 20, 'active'),
+    (2, 120.00, '2023-11-01', 15, 'active'),
+    (3, 80.00, '2023-09-30', 30, 'active');
 
--- Sample data for the 'complaint' table
-INSERT INTO `complaint` (`customer_id`, `tour_for_sale_id`, `description`, `creation_date`, `status`) VALUES
-    (1, 1, 'Complaint 1', NOW(), 'unresolved'),
-    (2, 2, 'Complaint 2', NOW(), 'resolved'),
-    (3, 3, 'Complaint 3', NOW(), 'unresolved');
+-- Insert values into the complaint table
+INSERT INTO `complaint` (`customer_id`, `tour_for_sale_id`, `description`, `creation_date`, `status`)
+VALUES
+    (2, 1, 'Complaint about Tour 1', NOW(), 'unresolved'),
+    (3, 2, 'Complaint about Tour 2', NOW(), 'unresolved');
 
--- Sample data for the 'feedback' table
-INSERT INTO `feedback` (`customer_id`, `description`, `creation_date`, `tour_for_sale_id`) VALUES
-    (1, 'Feedback 1', NOW(), 1),
-    (2, 'Feedback 2', NOW(), 2),
-    (3, 'Feedback 3', NOW(), 3);
+-- Insert values into the feedback table
+INSERT INTO `feedback` (`customer_id`, `description`, `creation_date`, `tour_for_sale_id`)
+VALUES
+    (1, 'Great tour!', NOW(), 1),
+    (4, 'Enjoyed the experience.', NOW(), 3);
 
--- Sample data for the 'employee' table
-INSERT INTO `employee` (`account_id`, `employee_name`, `status`) VALUES
-    (6, 'Employee 1', 'active'),
-    (7, 'Employee 2', 'suspended');
+-- Insert values into the employee table
+INSERT INTO `employee` (`account_id`, `employee_name`, `status`)
+VALUES
+    (4, 'Employee 1', 'active'),
+    (8, 'Employee 2', 'active');
 
--- Sample data for the 'booking' table
-INSERT INTO `booking` (`tour_id`, `customer_id`, `booking_date`, `status`) VALUES
+-- Insert values into the booking table
+INSERT INTO `booking` (`tour_id`, `customer_id`, `booking_date`, `status`)
+VALUES
     (1, 1, NOW(), 'paid'),
     (2, 2, NOW(), 'paid'),
-    (3, 3, NOW(), 'unpaid');
+    (3, 3, NOW(), 'paid');
+
